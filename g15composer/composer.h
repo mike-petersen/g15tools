e36825cf7205746a7751ac4fb9657b30f2b16edd
aneurysm9 <aneurysm9@f3f12dcf-2114-0410-a68b-d6aaca122e96>
2006-10-18 22:22:24 +0000

    Add parseCommandLine declaration to composer.h
    git-svn-id: https://g15tools.svn.sourceforge.net/svnroot/g15tools/trunk@117 f3f12dcf-2114-0410-a68b-d6aaca122e96

--- g15composer/composer.h
+++ g15composer/composer.h
@@ -12,6 +12,7 @@ void handleTextCommand(std::string const &input_line);
 void updateScreen(bool);
 int get_params(int*, std::string const &, int, int);
 void fifoProcessingWorkflow(bool is_script, string const &filename);
+void parseCommandLine(string cmdline);
 
 
 #endif /*COMPOSER_H_*/
