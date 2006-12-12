03243ba8118a696c17fcd124b9feddc638490798
aneurysm9 <aneurysm9@f3f12dcf-2114-0410-a68b-d6aaca122e96>
2006-12-12 18:43:57 +0000

    Update amarok examples and add album info
    git-svn-id: https://g15tools.svn.sourceforge.net/svnroot/g15tools/trunk@207 f3f12dcf-2114-0410-a68b-d6aaca122e96

--- g15composer/examples/amarok-g15-dcop_cli.pl
+++ g15composer/examples/amarok-g15-dcop_cli.pl
@@ -13,6 +13,8 @@ my $tmpStatus = `dcop amarok player status`;
 chomp $tmpStatus;
 my $status  :  shared = ( $tmpStatus > 1 ) ? 1 : 0;
 
+$SIG{TERM} = \&bye;
+
 open(CPIPE, ">>$controlPipe");
 print CPIPE "SN \"$pipe\"\n";
 close(CPIPE);
@@ -26,6 +28,8 @@ sub volUpdate {
 	print PIPE "DB 3 27 157 35 2 $vol 100 3\n";
 	print PIPE "MC 1\n";
 }
+
+&stoppedScreen();
 
 while($status == 0) {
 	$tmpStatus = `dcop amarok player status`;
@@ -49,8 +53,6 @@ chomp $trackTotalTime;
 chomp $trackCurTime;
 
 &initScreen();
-
-$SIG{TERM} = \&bye;
 
 my $progressThread = threads->create(\&progress);
 $progressThread->detach();
@@ -95,9 +97,11 @@ while(1) {
                         $status = 1;
 			initScreen();
                 } elsif ( /pause/ ) {
-                        $status = 0;
+                        $status = 2;
                 } else {
                         $status = 0;
+			stoppedScreen();
+			stoppedScreen();
                 }
         } elsif( /volumeChange/ ){
                 m/: (\d+)/;
@@ -116,29 +120,53 @@ sub bringToFront {
 	print PIPE "MP 2\n";
 }
 
+sub stoppedScreen {
+       print PIPE "MC 0\n";
+       print PIPE "PC 0\n";
+       print PIPE "DR 0 0 159 43 1 1\n";
+       print PIPE "DR 3 22 157 40 0 1\n";
+       print PIPE "PB 3 22 157 24 0\n";
+       print PIPE "FP 0 15 0 10 0 1 \"Playback Stopped\"\n";
+       print PIPE "MC 1\n";
+}
+
 sub initScreen {
 	print PIPE "PC 0\n";
 	print PIPE "DR 0 0 159 43 1 1\n";
 	print PIPE "DR 3 22 157 40 0 1\n";
 	print PIPE "PB 3 22 157 24 0\n";
-	print PIPE "FP 0 15 0 0 0 1 \"$artist\"\n";
-	print PIPE "FP 0 9 0 15 0 1 \"$title\"\n";
-	print PIPE "DB 3 27 157 35 2 $trackCurSecs $trackTotalSecs 3\n";
-	print PIPE "TO 0 35 0 1 \"    $trackCurTime / $trackTotalTime    \"\n";
+	print PIPE "MX 1\n";
+	print PIPE "FP 0 15 0 0 1 1 \"$artist\"\n";
+	print PIPE "FP 0 8 0 13 1 1 \"$album\"\n";
+	print PIPE "FP 0 7 0 23 1 1 \"$title\"\n";
+	print PIPE "DB 5 30 155 38 1 $trackCurSecs $trackTotalSecs 1\n";
+	print PIPE "TO 0 32 0 1 \"$trackCurTime / $trackTotalTime\"\n";
+	print PIPE "MX 0\n";
+	print PIPE "DL 5 29 5 39 1\n";
+	print PIPE "PS 5 29 0\n";
+	print PIPE "PS 5 39 0\n";
 	print PIPE "MC 1\n";
 }
 
 sub progress {
 	while(1) {
-		if($status > 0) {
+		if($status == 1) {
 			$trackCurSecs = `dcop amarok player trackCurrentTime`;
 			$trackCurTime = `dcop amarok player currentTime`;
 			chomp $trackCurSecs;
 			chomp $trackCurTime;
-			print PIPE "PB 5 27 155 35 0 1 1\n";
-			print PIPE "DB 3 27 157 35 2 $trackCurSecs $trackTotalSecs 3\n";
-			print PIPE "TO 0 35 0 1 \"    $trackCurTime / $trackTotalTime    \"\n";
+			print PIPE "PB 5 29 155 40 0 1 1\n";
+			print PIPE "MX 1\n";
+			print PIPE "DB 5 30 155 38 1 $trackCurSecs $trackTotalSecs 1\n";
+			print PIPE "TO 0 32 0 1 \"$trackCurTime / $trackTotalTime\"\n";
+			print PIPE "MX 0\n";
+			print PIPE "PB 5 29 5 39 1\n";
+			print PIPE "PS 5 29 0\n";
+			print PIPE "PS 5 39 0\n";
 			print PIPE "MC 1\n";
+               } elsif($status == 0) {
+                       stoppedScreen();
+                       $status = 2;
 		} elsif($status == -1) {
 			return;
 		}
