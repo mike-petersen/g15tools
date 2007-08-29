ee2ddec9d6c0f3caaf45737b8c79189c8733be54
aneurysm9 <aneurysm9@f3f12dcf-2114-0410-a68b-d6aaca122e96>
2007-08-29 04:13:19 +0000

    include stdlib.h and unistd.h in pixel.c
    git-svn-id: https://g15tools.svn.sourceforge.net/svnroot/g15tools/trunk@250 f3f12dcf-2114-0410-a68b-d6aaca122e96

--- libg15render/src/pixel.c
+++ libg15render/src/pixel.c
@@ -16,6 +16,8 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
+#include <stdlib.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <math.h>
 #include "libg15render.h"
